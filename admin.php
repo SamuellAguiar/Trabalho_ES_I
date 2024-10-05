5f3f9784c2f6b58ce384cefe31c962f901276fb6
Cauã Bandeira Nobre <44434295+CauaB@users.noreply.github.com>
2024-10-05 14:35:06 -0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,6 +5,62 @@ include_once("process/orders.php");
 
 ?>
 
+
+
+<!-- Estrutura HTML da Página Admin -->
+<div id="main-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Gerenciamento de Pedidos</h2>
+            </div>
+            <div class="col-md-12 table-container">
+            <form action="process/orders.php" method="POST" id="cancelForm">
+                <input type="hidden" name="type" value="delete">
+                
+                <div class="row">
+                    <?php foreach ($pizzas as $pizza) : ?>
+                        <div class="col-md-4 mb-4">
+                            <div class="card">
+                                <div class="card-body ">
+                                    <h5 class="card-title"><h3>Pedido #<?= $pizza["id"]?>:</h3></h5>
+                                    <br>
+                                        <p class="card-text"><strong>Tamanho:</strong> <?= $pizza["tamanho"]?></p>
+                                        <p class="card-text"><strong>Borda:</strong> <?= $pizza["borda"]?></p>
+                                        <p class="card-text"><strong>Massa:</strong> <?= $pizza["massa"]?></p>
+                                        <p class="card-text"><strong>Ingredientes:</strong>
+                                            <ul class="ingredientes-list-adm">
+                                                
+                                </div>    
+                            </div>    
+                        
+                </div>
+
+                    <?php endforeach; ?>
+                    
+            </div>
+        </div>
+    </div>
+                    <!-- Emissão de relatórios -->
+                    <div class="col-md-12">
+                    <h3 class="text-center">Relatórios</h3>
+                    </div>       
+                    <div class="row justify-content-center" id="relatorios">
+
+                    
+
+                    <div class="col-md-4 mb-4 text-center">
+                        <form action="process/relatorio.php" method="post">
+                            <input type="hidden" name="relatorio" value="ingredientes">
+                            <button type="submit" class="btn btn-primary">Relatório de ingredientes</button>
+                        </form>
+                    </div>
+
+                 
+            </div>
+
+
+</div>
                                                 
             <div class="col-md-4 mb-4 text-center">
                                     <form action="process/relatorio.php" method="post">
