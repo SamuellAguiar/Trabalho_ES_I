5601a792dccee02a5c5a3557c9c6b522f0a110f1
Cauã Bandeira Nobre <44434295+CauaB@users.noreply.github.com>
2024-10-05 14:30:04 -0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -60,7 +60,23 @@ include_once("process/orders.php");
 
 
 </div>
+                                                
+            <div class="col-md-4 mb-4 text-center">
+                                    <form action="process/relatorio.php" method="post">
+                                        <input type="hidden" name="relatorio" value="usuarios">
+                                        <button type="submit" class="btn btn-primary">Relatório de usuários</button>
+                                    </form>
+                                </div>
+                        </div>
+                                                            
+             <div class="col-md-4 mb-4 text-center">
+                                    <form action="process/relatorio.php" method="post">
+                                        <input type="hidden" name="relatorio" value="pedidos">
+                                        <button type="submit" class="btn btn-primary">Relatório de pedidos</button>
+                                    </form>
+                                </div>
 
+                                                
 
 
 <?php
